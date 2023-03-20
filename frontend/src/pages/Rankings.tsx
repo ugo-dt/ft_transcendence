d0b522e5b047bd540181de378213eb6d4e97ee2b
Naerhy <naerhy@protonmail.com>
2023-03-20 08:58:38 +0100

    Implement basic leaderboard

--- frontend/src/pages/Rankings.tsx
+++ frontend/src/pages/Rankings.tsx
@@ -5,14 +5,77 @@
 //
 //  See their current ranking
 
+import React from "react";
+
+import {
+  Container,
+  CssBaseline,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from "@mui/material";
+
+const placeholder = [
+  {
+    id: 0,
+    rank: 3,
+    nickname: "Quentin",
+    points: 345
+  },
+  {
+    id: 1,
+    rank: 1,
+    nickname: "Sarah",
+    points: 2554
+  },
+  {
+    id: 2,
+    rank: 4,
+    nickname: "Antoine",
+    points: 1
+  },
+  {
+    id: 3,
+    rank: 2,
+    nickname: "Marie",
+    points: 654
+  },
+];
+
 function Rankings() {
-	return (
-    <div className="Rankings">
-		  <h1>
-        This is the Rankings page.
-      </h1>
-    </div>
-	);
+  placeholder.sort((a, b) => a.rank - b.rank);
+  // ADD CSSBASELINE TO ROOT APP IN ORDER TO RESET BROWSER CSS?
+  return (
+    <Container maxWidth="md">
+      <CssBaseline /> 
+      <TableContainer component={Paper} sx={{mt: 10}}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Rank</TableCell>
+              <TableCell align="center">Nickname</TableCell>
+              <TableCell align="center">Points</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {placeholder.map(item => {
+              return (
+                <TableRow key={item.id}>
+                  <TableCell align="center">{item.rank}</TableCell>
+                  <TableCell align="center">{item.nickname}</TableCell>
+                  <TableCell align="center">{item.points}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
 
 export default Rankings;

815bcb3a233bed00df961ac2d96137534e6a4a27
Naerhy <naerhy@protonmail.com>
2023-04-20 16:17:02 +0200

    Use redis for session-storage
    Do not forget to install redis-server on your machine.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,18 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
+import { createClient } from 'redis';
+import RedisStore from 'connect-redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   const clientPort = configService.get('CLIENT_PORT');
+
+  const redisClient = createClient();
+  redisClient.connect().catch(console.error);
+  const redisStore = new RedisStore({client: redisClient});
 
   app.setGlobalPrefix('api');
   app.enableCors({
@@ -20,6 +26,7 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   app.use(session({
+    store: redisStore,
     secret: "a-secret-string",
     resave: false,
     saveUninitialized: false,
